0bd6b6b6df905dc7747d969b5fc107f55ecd439c
Aki Kaivola <aki.kaivola@reaktor.com>
2022-10-12 22:25:20 +0300

    List charges

--- app/routes/chargetracker/index.tsx
+++ app/routes/chargetracker/index.tsx
@@ -1,19 +1,15 @@
+import { json } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react"
-import { json } from "stream/consumers"
+import type { ChargeEvent} from "~/models/chargeevents.server";
+import { getChargeEvents } from "~/models/chargeevents.server"
 import type { User } from "~/models/user.server"
 import { requireUserId } from "~/session.server"
-import { useUser } from "~/utils"
 
-type Provider = 'office' | 'lidl' | 'abc' | 'recharge' | 'other'
-
-const getChargeEvents = (userId: number) => {
-
-}
-
-const loader = async ({ request }: LoaderArgs) => {
+export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request)
-  const chargeEvents = getChargeEvents({ userId })
-  return json({  })
+  const chargeEvents = await getChargeEvents({ userId })
+  return json({ chargeEvents })
 }
 
 interface ChargeTrackerIndexPageProps {
@@ -21,8 +17,14 @@ interface ChargeTrackerIndexPageProps {
 }
 
 export default function ChargeTrackerIndexPage({ user }: ChargeTrackerIndexPageProps) {
-  const { chargeEvents }  = useLoaderData<typeof loader>
-  const user = useUser()
+  const { chargeEvents } = useLoaderData<typeof loader>()
 
-  return (<div>index</div>)
+  {chargeEvents.map(({ date, kiloWattHours, pricePerKiloWattHour, provider }) => (
+    <div>
+      <div>{date}</div>
+      <div>{kiloWattHours}</div>
+      <div>{pricePerKiloWattHour}</div>
+      <div>{provider}</div>
+    </div>
+  ))}
 }
