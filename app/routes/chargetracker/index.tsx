8e5174d2e41067a6e4e59ad2513499cedcdb6ef7
Aki Kaivola <aki.kaivola@reaktor.com>
2022-11-03 17:27:08 +0200

    Add handler for inserting charge event to db

--- app/routes/chargetracker/index.tsx
+++ app/routes/chargetracker/index.tsx
@@ -1,13 +1,15 @@
+import { Prisma } from '@prisma/client';
 import type { ActionArgs, LoaderArgs, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Form, useFetcher, useLoaderData } from '@remix-run/react';
 import _ from 'lodash';
-import type { FormEvent, SyntheticEvent} from 'react';
+import type { SyntheticEvent } from 'react';
 import { useState } from 'react';
-import React from 'react';
-import { getChargeEvents } from '~/models/chargeevents.server';
+import { createChargeEvent, getChargeEvents, upsertChargeEvent } from '~/models/chargeevents.server';
 import { requireUserId } from '~/session.server';
 import { logger } from '../../logger.server';
+import type { Provider, Provider } from '../../models/providers.server';
+import { getProviderCounts } from '../../models/providers.server';
 
 const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' }
 
@@ -21,19 +23,32 @@ export async function loader({ request }: LoaderArgs) {
     kiloWattHours: event.kiloWattHours.toNumber(),
     pricePerCharge: event.pricePerCharge.toNumber(),
   }))
-  return json({ chargeEvents })
+  const providers = (await getProviderCounts(userId))
+  return json({ chargeEvents, providers })
 }
 
 export async function action({request}: ActionArgs) {
+  const userId = await requireUserId(request)
   const formData = await request.formData()
   const { _action, ...values } = Object.fromEntries(formData)
 
   logger.info(`action: ${_action} ${JSON.stringify(values)}`)
 
-  return null
-  // upsertChargeEvent({
+  if ('new' === _action) {
+    const { date, kiloWattHours, pricePerCharge, provider } = values
+    const [day, month, year] = date.toString().split('.')
+    const result = await createChargeEvent({
+      userId,
+      date: new Date(Date.parse(`${year}-${month}-${day}T00:00:00Z`)),
+      kiloWattHours: new Prisma.Decimal(kiloWattHours.toString()), 
+      pricePerCharge: new Prisma.Decimal(pricePerCharge.toString()),
+      provider: provider.toString()
+    })
 
-  // })
+    logger.info(JSON.stringify(result))
+  }
+
+  return null
 }
 
 export const meta: MetaFunction = () => {
@@ -57,73 +72,78 @@ function DateAdjustButton(props: { value: number, getter: Date, setter: (newValu
   )
 }
 
-export default function ChargeTrackerIndexPage() {
-  const loaderData = useLoaderData<typeof loader>()
-  const chargeEvents = loaderData.chargeEvents
+interface NewChargeEntryProps {
+  providers: Provider[]
+}
 
+function NewChargeEntry(props: NewChargeEntryProps) {
+  const { providers } = props
+
+  const [date, setDate] = useState(new Date())
+  const [kiloWattHours, setKiloWattHours] = useState(0)
+  const [price, setPrice] = useState(0)
+  const [provider, setProvider] = useState(_.first(providers))
+
+  return (
+    <Form id='new' method='post'>
+      <input type='hidden' name='_action' value='new' form='new' readOnly />
+      <div className='grid grid-cols-4'>
+        <div className='justify-self-center grid'>
+          <div className='grid grid-cols-1 w-1/2'>
+            <DateAdjustButton value={-1} getter={date} setter={setDate} />
+          </div>
+          <input type='text' className='bg-black' form='new' name='date' size={10} defaultValue={date.toLocaleDateString('fi-FI', options)}/>
+          <div className='grid grid-cols-1 w-1/2'>
+            <DateAdjustButton value={1} getter={date} setter={setDate} />
+          </div>
+        </div>
+        <div className='justify-self-center grid'>
+          <div className='grid grid-cols-2'>
+            <AdjustButton value={-1} getter={kiloWattHours} setter={setKiloWattHours} />
+            <AdjustButton value={-0.1} getter={kiloWattHours} setter={setKiloWattHours} />
+          </div>
+          <span className='grid grid-cols-2 items-center'>
+            <input type='text' className='bg-black justify-self-center text-center' form='new' name='kiloWattHours' size={6} defaultValue={kiloWattHours}/>{' '}kWh
+          </span>
+          <div className='grid grid-cols-2'>
+            <AdjustButton value={1} getter={kiloWattHours} setter={setKiloWattHours} />
+            <AdjustButton value={0.1} getter={kiloWattHours} setter={setKiloWattHours} />
+          </div>
+        </div>
+        <div className='justify-self-center grid'>
+          <div className='grid grid-cols-2'>
+            <AdjustButton value={-1} getter={price} setter={setPrice} />
+            <AdjustButton value={-0.1} getter={price} setter={setPrice} />
+          </div>
+          <span className='grid grid-cols-2 items-center'>
+            <input type='text' className='bg-black justify-self-center text-center' form='new' name='pricePerCharge' size={6} defaultValue={price}/>{' '}e
+          </span>
+          <div className='grid grid-cols-2'>
+            <AdjustButton value={1} getter={price} setter={setPrice} />
+            <AdjustButton value={0.1} getter={price} setter={setPrice} />
+          </div>
+        </div>
+        <div className='grid grid-rows-3'>
+          <div className='dropdown dropdown-left row-start-1'>
+            <input type='hidden' value={provider?.name || ''} name='provider' form='new' readOnly />
+            <label tabIndex={0} className='btn btn-sm btn-primary rounded p-1 m-1'>{provider?.name || '???'}</label>
+            <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded w-52'>
+              { providers.map(p =>
+                <li key={p.name} className='rounded'><a onClick={() => setProvider(p)}>{p.name}</a></li>
+              )}
+            </ul>
+          </div>
+        </div>
+        <button type='submit' className='my-8 col-span-4 justify-self-center btn btn-primary rounded' value='insert'>insert</button>
+      </div>
+    </Form>
+  )
+}
+
+export default function ChargeTrackerIndexPage() {
+  const { chargeEvents, providers } = useLoaderData<typeof loader>()
   const { state, type, submission, data, submit, load } = useFetcher()
   console.log(state, type, submission, data)
-
-  const NewChargeEntry: React.FC<{}> = (props: {}) => {
-    const currentDate = new Date().toLocaleDateString('fi-FI', options)
-    const formSubmit = (e: FormEvent<HTMLFormElement>) => { 
-      e.preventDefault()
-      console.log(e.target)
-      submit(new FormData(e.currentTarget), { method: 'post' })
-    }
-
-    const [date, setDate] = useState(new Date())
-    const [kiloWattHours, setKiloWattHours] = useState(0)
-    const [price, setPrice] = useState(0)
-
-    return (
-      <Form id='new' method='post'>
-        <div className='grid grid-cols-4'>
-          <div className='justify-self-center'>Date</div>
-          <div className='justify-self-center'>kWh</div>
-          <div className='justify-self-end'>e / charge</div>
-          <div>provider</div>
-        </div>
-        <div className='grid grid-cols-4'>
-          <div className='justify-self-center grid'>
-            <div className='grid grid-cols-1 w-1/2'>
-              <DateAdjustButton value={-1} getter={date} setter={setDate} />
-            </div>
-            <input type='text' className='bg-black' form='new' name='date' size={10} value={date.toLocaleDateString('fi-FI', options)}/>
-            <div className='grid grid-cols-1 w-1/2'>
-              <DateAdjustButton value={1} getter={date} setter={setDate} />
-            </div>
-          </div>
-          <div className='justify-self-center grid'>
-            <div className='grid grid-cols-2'>
-              <AdjustButton value={-1} getter={kiloWattHours} setter={setKiloWattHours} />
-              <AdjustButton value={-0.1} getter={kiloWattHours} setter={setKiloWattHours} />
-            </div>
-            <input type='text' className='bg-black justify-self-center text-center' form='new' name='kiloWattHours' size={6} value={kiloWattHours}/>
-            <div className='grid grid-cols-2'>
-              <AdjustButton value={1} getter={kiloWattHours} setter={setKiloWattHours} />
-              <AdjustButton value={0.1} getter={kiloWattHours} setter={setKiloWattHours} />
-            </div>
-          </div>
-          <div className='justify-self-center grid'>
-            <div className='grid grid-cols-2'>
-              <AdjustButton value={-1} getter={price} setter={setPrice} />
-              <AdjustButton value={-0.1} getter={price} setter={setPrice} />
-            </div>
-            <input type='text' className='bg-black justify-self-center text-center' form='new' name='pricePerCharge' size={6} value={price}/>
-            <div className='grid grid-cols-2'>
-              <AdjustButton value={1} getter={price} setter={setPrice} />
-              <AdjustButton value={0.1} getter={price} setter={setPrice} />
-            </div>
-          </div>
-          <div className='grid'>
-            <input type='text' className='bg-black place-content-center text-center' form='new' name='provider' size={8} defaultValue={'office'}/>
-          </div>
-          <button type='submit' className='my-8 col-span-4 justify-self-center btn btn-primary rounded' value='insert'>insert</button>
-        </div>
-      </Form>
-    )
-  }
 
   const total = chargeEvents.reduce(({kWh, price, count}, ce) => {
     return {
@@ -135,7 +155,7 @@ export default function ChargeTrackerIndexPage() {
 
   return (
     <div className='container mx-auto p-4'>
-      <NewChargeEntry />
+      <NewChargeEntry providers={providers} />
       <div className='grid grid-cols-3 my-2 text-lg font-bold'>
         <div>
           {total.count} charges
