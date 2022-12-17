a440d748d3a1b3057f2bd0b333f88cbe5e904376
Aki Kaivola <aki.kaivola@reaktor.com>
2022-12-17 21:37:24 +0200

    Remove h-full

--- app/root.tsx
+++ app/root.tsx
@@ -30,12 +30,12 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function App() {
   return (
-    <html data-theme="forest" lang="en" className="h-full">
+    <html data-theme="forest" lang="en" className="">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className="h-full font-mono text-green-400">
+      <body className="font-mono text-green-400">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
